b13656f8ae6e45fc21c7bf9f8e675e24433eece0
Gayasri72 <gayasripethum1@gmail.com>
2025-04-20 11:36:09 +0530

    developing about section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { assets } from "@/assets/assets";
 const Navbar = () => {
+  const [isScroll, setIsScroll] = useState(false);
   const sideMenuRef = useRef();
   const openMenu = () => {
     sideMenuRef.current.style.transform = "translateX(-16rem)";
@@ -9,30 +10,50 @@ const Navbar = () => {
   const closeMenu = () => {
     sideMenuRef.current.style.transform = "translateX(16rem)";
   };
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      
+      if (scrollY > 50) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    } );
+  }, []);
   return (
     <>
-      <div className="fixed top-0 right-0 w-11/12 z-10 translate-y-[-80%]">
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
         <Image src={assets.header_bg_color} alt="header" className="w-full " />
       </div>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
         <a href="#top" className="text-3xl  cursor-pointer mr-14">
           Gayasri .
         </a>
         <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50 ">
           <li>
-            <a href="#top">Home</a>
+            <a href="#top" className="font-Ovo">
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about">About Me</a>
+            <a href="#about" className="font-Ovo">
+              About Me
+            </a>
           </li>
           <li>
-            <a href="#services">Services</a>
+            <a href="#services" className="font-Ovo">
+              Services
+            </a>
           </li>
           <li>
-            <a href="#work">My Work</a>
+            <a href="#work" className="font-Ovo">
+              My Work
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact Me</a>
+            <a href="#contact" className="font-Ovo">
+              Contact Me
+            </a>
           </li>
         </ul>
         <div className="flex items-center gap-4">
